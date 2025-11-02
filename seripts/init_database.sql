e9dba40cf6ac8f93a11b11e7a840001c73f290a5
rahul769014 <111835631+rahul769014@users.noreply.github.com>
2025-11-02 11:16:00 +0530

    Update init_database.sql

--- seripts/init_database.sql
+++ seripts/init_database.sql
@@ -1,18 +1,39 @@
 /*
 ===============================================================================
-Stored Procedure: Load Bronze Layer (Source -> Bronze)
+Create Database and Schema
 ===============================================================================
 Script Purpose:
-    This stored procedure loads data into the 'bronze' schema from external CSV files. 
-    It performs the following actions:
-    - Truncates the bronze tables before loading data.
-    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
+    This script creates a new database named 'Datawarehouse' after checking if it already exists.
+    If a database exists, it is dropped and recreate. Additionally, the script sets up three schemas
+	within the database: 'Bromze','Silver',and 'gold' .
 
-Parameters:
-    None. 
-	  This stored procedure does not accept any parameters or return any values.
-
-Usage Example:
-    EXEC bronze.load_bronze;
-===============================================================================
+WARNING:
+	Running this script will drop the entore 'DataWarehouse' database if it exist.
+All data in the database will be permanently deleted. Proceed with caution and ensure you have proper backups before running this script.
 */
+
+ USE master;
+ GO
+
+ -- Drop and recreate the 'DataWarehouse' database
+IF Exit (SELECT 1 FROM  sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+	
+-- Create Database 'DataWarehouse'
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+GO
+	
+CREATE SCHEMA bronze;
+GO
+	
+CREATE SCHEMA silver;
+GO
+	
+CREATE SCHEMA gold;
+GO
